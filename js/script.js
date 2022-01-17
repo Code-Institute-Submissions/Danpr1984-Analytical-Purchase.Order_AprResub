0fb8992368715edf06dd1c1a8161d83997c8c118
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-01-17 12:36:14 +0000

    Add EventListener change to JS

--- js/script.js
+++ js/script.js
@@ -1,55 +1,69 @@
-let shrimp = { 
+
+
+const shrimp = { 
     share: 0.3,  
     weight: 0.33,
     unitCost: 5
 };
 
-let snapper = {
+const snapper = {
     share: 0.2,  
     weight: 0.33,
     unitCost: 4
-
 };
 
-let seabass = {
+const seabass = {
     hare: 0.2,  
     weight: 0.33,
     unitCost: 4
-
 };
 
-let chicken = {
+const chicken = {
     hare: 0.2,  
     weight: 0.33,
     unitCost: 4
-
 };
 
-let ribeye = {
+const ribeye = {
     share: 0.1,  
     weight: 0.5,
     unitCost: 4
-
 };
 
-function contentLoad();
+let operand1Shrimp = document.getElementById('operand1-shrimp');
+operand1Shrimp.addEventListener('change', function(e) {
+    console.log(parseInt(operand1Shrimp.value));
+} )
+	
+let operand2Shrimp = operand1Shrimp.value;
+    
 
-function contentLoad2();
+    
+
+
+function contentLoad() {}
+
+
+
 /**
  * This function should execute all the operations and show results in chart
  */
-function runCalculations (); 
+function runCalculations () {}
+
 
 /**
  * This function should divide the total amount of dishes expected to be sold with the sells share of each dish
  */
-function calculateDishesByProduct();
+function calculateDishesByProduct() {}
+
 
 /**
  * This function should multiply the total amount of dishes expected to be sold per product with the amount of product contained in each dish
  */
-function calculateAmount();
+function calculateAmountLb() {}
 
 /**
  * This function should substract the product on stock from the total amount of product needed and multiply the result by the unit cost of each product */
-function calculateOrder();
+function calculateOrder() {}
+
+
