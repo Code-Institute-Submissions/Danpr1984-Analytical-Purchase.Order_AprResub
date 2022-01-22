89474c8fa9be9d3abbf6b204ff59c0a667f552a1
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-01-22 18:21:29 +0000

    Add a contentload event listener function to javascript file to add image of the chef 2 seconds after the page has loaded. Also added css styling to all html files

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@
  * This function picks up the values from the calculate.html so they can be used to make operations with JavaScript files that will reflect in the forms in the order.html file
  */
 
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(function () {
+        document.getElementById("load-content").style.visibility = "visible";
+    }, 2500);
+})
+
+
 let myButton = document.getElementById("continue");
 myButton.addEventListener("click", function Continue() {
 
@@ -21,5 +28,5 @@ myButton.addEventListener("click", function Continue() {
     const shoppingList = [start, end, dishes, createdBy, operand1Shrimp, operand1Snapper, operand1Seabass, operand1Chicken, operand1Ribeye];
     window.location = '../order.html?shoppingList=' + shoppingList;
 
-   
+
 });
